8e4cae377c63c0fcc52311b8dade120ed3bd1f3e
OTX <asmaotsmane05@gmail.com>
2026-08-14 20:44:08 +0100

    csrf token.2

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://nyra-backend-a0qr.onrender.com/api/:path*",
+      },
+    ]
+  },
 }
 
 export default nextConfig
